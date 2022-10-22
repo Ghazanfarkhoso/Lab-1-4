4faacfe0432f21de31b8c8621c2e9f7038c0846b
Ghazanfar Hussain <116442321+Ghazanfarkhoso@users.noreply.github.com>
2022-10-22 20:56:51 +0500

    Create variableslab1.java

---
+++ variableslab1.java
@@ -0,0 +1,23 @@
+class variable{
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println("the number is integer"+a);
+        System.out.println("the number is integer"+b);
+        double c=9.998;
+        double  d=0.6;
+
+System.out.println("the number is double data type"+c);
+System.out.println("the number is float data type" +d);
+
+char e='a';
+char f='b';
+System.out.println('e');
+System.out.println('f');
+
+
+
+}
+
+
+    }
